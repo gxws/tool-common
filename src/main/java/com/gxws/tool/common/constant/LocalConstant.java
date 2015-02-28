bed5bca8a92df0525105f898cf74be0bbaa75190
朱伟亮 <zhuwl120820@gxwsxx.com>
2015-02-28 16:54:37 +0800

    add：添加了全局属性常量

---
+++ src/main/java/com/gxws/tool/common/constant/LocalConstant.java
@@ -0,0 +1,12 @@
+package com.gxws.tool.common.constant;
+
+/**
+ * 运行本地参数
+ * 
+ * @author 朱伟亮
+ * @create 2015年2月28日下午3:41:54
+ *
+ */
+public class LocalConstant {
+	public static String IP;// 服务器网卡IP地址
+}
---
+++ src/main/java/com/gxws/tool/common/constant/ProjectConstant.java
@@ -0,0 +1,16 @@
+package com.gxws.tool.common.constant;
+
+/**
+ * 项目信息
+ * 
+ * @author 朱伟亮
+ * @create 2015年2月27日下午5:33:56
+ *
+ */
+public class ProjectConstant {
+
+	public static String NAME = "";// 项目名
+
+	public static String ENV = "";// 项目环境
+
+}
