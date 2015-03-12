e402c8f8bcd621dda7876070548e9999431e54f3
朱伟亮 <zhuwl120820@gxwsxx.com>
2015-03-12 17:27:28 +0800

    update：根据日志收集的要求，调整了变量结构，统一了项目变量名称

--- src/main/java/com/gxws/tool/common/constant/ProjectConstant.java
+++ src/main/java/com/gxws/tool/common/constant/ProjectConstant.java
@@ -1,7 +1,10 @@
 package com.gxws.tool.common.constant;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * 项目信息
+ * 项目信息，属性值
  * 
  * @author 朱伟亮
  * @create 2015年2月27日下午5:33:56
@@ -9,10 +12,42 @@ package com.gxws.tool.common.constant;
  */
 public class ProjectConstant {
 
-	public static String NAME = "";// 项目名
+	public static String VALUE_PROJECT_NAME = "";// 项目名
 
-	public static String ENV = "";// 项目环境
+	public static String VALUE_PROJECT_ENV = "";// 项目环境
 
-	public static String VERSION = "";// 项目版本
+	public static String VALUE_PROJECT_VERSION = "";// 项目版本
+
+	public static String VALUE_PROJECT_IP = "";// 项目运行网卡IP地址
+
+	public static String VALUE_PROJECT_PORT = "";// 项目运行实例端口号
+
+	public static final String NAME_PROJECT_NAME = "project.name";
+
+	public static final String NAME_PROJECT_ENV = "project.env";
+
+	public static final String NAME_PROJECT_VERSION = "project.version";
+
+	public static final String NAME_PROJECT_IP = "project.ip";
+
+	public static final String NAME_PROJECT_PORT = "project.port";
+
+	public static Map<String, String> map = new HashMap<>();
+
+	public static void put(String key, String value) {
+		map.put(key, value);
+	}
+
+	public static String get(String key) {
+		return map.get(key);
+	}
+
+	public static void putAll(Map<String, String> m) {
+		map.putAll(m);
+	}
+
+	public static Map<String, String> getAll() {
+		return map;
+	}
 
 }
